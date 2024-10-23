6fe232db7600966728ec4f580d1061d6a548444f
weet <115834022+weet-ops@users.noreply.github.com>
2024-10-23 14:22:51 -0500

    dont have a cow man

---
+++ nitroGen.py
@@ -0,0 +1,27 @@
+import random
+import string
+import time
+from discord_webhook import DiscordWebhook
+
+
+WEBHOOK = "DISCORD.COM/WEBHOOK/WHATEVERTHELINKWASIDKIFORGOTLMAOTHISISATIMEWASTERDONTACTUALLYRUNTHISANDEXPECTSOMETHINGYOUFUNKYBASEMENTDWELLERYOU"
+
+def generateCode(length):
+    # THE MAIN ACT
+    letters = string.ascii_lowercase
+    letters2 = string.ascii_uppercase
+    numbers = string.digits
+    result_str = ''.join(random.choice(letters) + random.choice(numbers) + random.choice(letters2) for i in range(length))
+    print("Generated code", "is:", "https://discord.gift/" + result_str)
+
+    webhook = DiscordWebhook(url=WEBHOOK, content=result_str)
+    response = webhook.execute()
+
+
+
+print("HEY THERE VERY EPIC SKID MACHINE WHO HAD NOTHING TO DO ON THE 21st NIGHT OF SEPTEMBER, LEAVE THIS RUNNING FOR 8 OCTILLION YEARS FOR FREE NITRO (for legal reasons this is a joke.)")
+
+while True:
+    time.sleep(2)
+    generateCode(19)
+
